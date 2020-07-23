c9bb07284d1fb5e89f0f0e674221fae76f33eb99
Martin lewis <martin@mdpdesigns.co.uk>
2020-07-23 22:28:33 +0100

    Some more dice changes.

--- Dice.php
+++ Dice.php
@@ -1,18 +1,11 @@
-<?php 
-
-$dice_1 = rand(1, 6);
-$dice_2 = rand(1, 6);
-
-$total = $dice_1 + $dice_2;
-
-echo "Dice 1 is " . $dice_1 . "</br>";
-echo "Dice 2 is " . $dice_2 . "</br>";
-
-echo "Total of the two dice together is " . $total . "</br>";
-
-if ($total == 7 || $total == 11) {
-    echo "You're a WINNER BABY!!!";
-} else if ($total == 2 || $total == 3 || $total == 12) {
-    echo "YOU CRAPPED OUT!!!";
-}
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Craps Game</title>
+</head>
+<body>
+    <button onclick="location='DiceRoll.php'">Click Me!</button>
+</body>
+</html>
---
+++ DiceRoll.php
@@ -0,0 +1,23 @@
+<?php 
+
+diceRoll();
+
+function diceRoll () {
+    $dice_1 = rand(1, 6);
+    $dice_2 = rand(1, 6);
+    
+    $total = $dice_1 + $dice_2;
+    
+    echo "Dice 1 is " . $dice_1 . "</br>";
+    echo "Dice 2 is " . $dice_2 . "</br>";
+    
+    echo "Total of the two dice together is " . $total . "</br>";
+    
+    if ($total == 7 || $total == 11) {
+        echo "You're a WINNER BABY!!!";
+    } else if ($total == 2 || $total == 3 || $total == 12) {
+        echo "YOU CRAPPED OUT!!!";
+    } else {
+        echo "Re-Roll!!!!!";
+    }
+}
